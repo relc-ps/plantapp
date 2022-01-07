1c56494ef29302a8a60237663cdad1d73d58af7d
Ricardo Camana <geral.relc@gmail.com>
2022-01-07 14:50:22 +0100

    welcome

--- navigation/index.js
+++ navigation/index.js
@@ -1,13 +1,8 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
-import Welcome from "./screens/Welcome";
-import Login from "./screens/Login";
-import Explore from "./screens/Explore";
-import Browser from "./screens/Browser";
-import Product from "./screens/Product";
-import Settings from "./screens/Settings";
 import { Image } from "react-native";
+import Welcome from "./../screens/Welcome";
 
 const screens = createStackNavigator(
   {
